32ad52e229e22a46c882d5c3134bbb67dc95a512
Amitoj Singh <iamamitoj@gmail.com>
2017-08-27 15:27:44 +0530

    fix: Quick fix

--- Sorting/MergeSortBottomUp.java
+++ Sorting/MergeSortBottomUp.java
@@ -1,25 +1,34 @@
 public class MergeSortBottomUp {
 
+    private static Comparable[] inputAux;
+
     public static void sort(Comparable[] inputArg) {
-        int size = inputArg.length;
-        for (int i = 1; i < size; i += i) {
-            for (int j = 0; j < size - i; j += i + i) {
-                merge(inputArg, j, j + i - 1, Math.min(j + i + i - 1, size - 1));
+
+        int length = inputArg.length;
+        inputAux = new Comparable[length];
+
+        for (int i = 1; i < length; i += i) {
+            for (int j = 0; j < length - i; j += i + i) {
+                merge(inputArg, j, j + i - 1, Math.min(j + i + i - 1, length - 1));
             }
         }
     }
 
-    // Merging into single array.
+    // Merging into single array
     public static void merge(Comparable[] inputArg, int start, int mid, int end) {
 
-        Comparable[] input = inputArg.clone();
+        for (int k = start; k <= end; k++) {
+            inputAux[k] = inputArg[k];
+        }
+
         int i = start;
         int j = mid + 1;
+
         for (int k = start; k <= end; k++) {
-            if (i > mid)                             inputArg[k] = input[j++];
-            else if (j > end)                        inputArg[k] = input[i++];
-            else if (isLessThan(input[j], input[i])) inputArg[k] = input[j++];
-            else                                     inputArg[k] = input[i++];
+            if      (i > mid)                              inputArg[k] = inputAux[j++];
+            else if (j > end)                              inputArg[k] = inputAux[i++];
+            else if (isLessThan(inputAux[j], inputAux[i])) inputArg[k] = inputAux[j++];
+            else                                           inputArg[k] = inputAux[i++];
         }
     }
 

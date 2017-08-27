e447360f291c6d5fbabc616a285313015d661f4d
Amitoj Singh <iamamitoj@gmail.com>
2017-08-27 15:27:59 +0530

    fix: Quick fix

--- Sorting/Test.java
+++ Sorting/Test.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test {
@@ -23,62 +24,96 @@ public class Test {
         }
         bufferedReader.close();
 
-        Integer[] inputArray = new Integer[input.size()];
+        int length = input.size();
+        Integer[] inputArray = new Integer[length];
         for (int i = 0; i < input.size(); i++) {
             String temp = input.get(i);
             inputArray[i] = Integer.parseInt(temp);
         }
 
-        Stopwatch stopwatch = new Stopwatch();
         long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
+        // RandomShuffle randomShuffle = new RandomShuffle();
+
         SelectionSort selectionSort = new SelectionSort();
-        SelectionSortTwoThreaded selectionSortTwoThreaded = new SelectionSortTwoThreaded();
-        InsertionSort insertionSort = new InsertionSort();
-        ShellSort shellSort = new ShellSort();
-        RandomShuffle randomShuffle = new RandomShuffle();
-        MergeSort mergeSort = new MergeSort();
-        MergeSortAux mergeSortAux = new MergeSortAux();
-        MergeSortImproved mergeSortImproved = new MergeSortImproved();
-        MergeSortBottomUp mergeSortBottomUp = new MergeSortBottomUp();
-
-        stopwatch.start();
-
-        Integer[] inputArray1 = inputArray.clone();
+        Integer[] inputArray1 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch1 = new Stopwatch();
+        stopwatch1.start();
         selectionSort.sort(inputArray1);
+        stopwatch1.stop();
+        System.out.println("Time SelectionSort            = " + stopwatch1.getTime() + "ms");
         isSorted(inputArray1);
-        Integer[] inputArray2 = inputArray.clone();
+
+        SelectionSortTwoThreaded selectionSortTwoThreaded = new SelectionSortTwoThreaded();
+        Integer[] inputArray2 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch2 = new Stopwatch();
+        stopwatch2.start();
         selectionSortTwoThreaded.sort(inputArray2);
+        stopwatch2.stop();
+        System.out.println("Time SelectionSortTwoThreaded = " + stopwatch2.getTime() + "ms");
         isSorted(inputArray2);
-        Integer[] inputArray3 = inputArray.clone();
+
+        InsertionSort insertionSort = new InsertionSort();
+        Integer[] inputArray3 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch3 = new Stopwatch();
+        stopwatch3.start();
         insertionSort.sort(inputArray3);
+        stopwatch3.stop();
+        System.out.println("Time InsertionSort            = " + stopwatch3.getTime() + "ms");
         isSorted(inputArray3);
-        Integer[] inputArray4 = inputArray.clone();
+
+        ShellSort shellSort = new ShellSort();
+        Integer[] inputArray4 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch4 = new Stopwatch();
+        stopwatch4.start();
         shellSort.sort(inputArray4);
+        stopwatch4.stop();
+        System.out.println("Time ShellSort                = " + stopwatch4.getTime() + "ms");
         isSorted(inputArray4);
-        Integer[] inputArray5 = inputArray.clone();
+
+        MergeSort mergeSort = new MergeSort();
+        Integer[] inputArray5 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch5 = new Stopwatch();
+        stopwatch5.start();
         mergeSort.sort(inputArray5);
+        stopwatch5.stop();
+        System.out.println("Time MergeSort                = " + stopwatch5.getTime() + "ms");
         isSorted(inputArray5);
-        Integer[] inputArray6 = inputArray.clone();
-        mergeSortAux.sort(inputArray6);
+
+        MergeSortImproved mergeSortImproved = new MergeSortImproved();
+        Integer[] inputArray6 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch6 = new Stopwatch();
+        stopwatch6.start();
+        mergeSortImproved.sort(inputArray6);
+        stopwatch6.stop();
+        System.out.println("Time MergeSortImproved        = " + stopwatch6.getTime() + "ms");
         isSorted(inputArray6);
-        Integer[] inputArray7 = inputArray.clone();
-        mergeSortImproved.sort(inputArray7);
+
+        MergeSortBottomUp mergeSortBottomUp = new MergeSortBottomUp();
+        Integer[] inputArray7 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch7 = new Stopwatch();
+        stopwatch7.start();
+        mergeSortBottomUp.sort(inputArray7);
+        stopwatch7.stop();
+        System.out.println("Time MergeSortBottomUp        = " + stopwatch7.getTime() + "ms");
         isSorted(inputArray7);
-        Integer[] inputArray8 = inputArray.clone();
-        mergeSortBottomUp.sort(inputArray8);
+
+        QuickSort quickSort = new QuickSort();
+        Integer[] inputArray8 = Arrays.copyOf(inputArray, length);
+        Stopwatch stopwatch8 = new Stopwatch();
+        stopwatch8.start();
+        quickSort.sort(inputArray8);
+        stopwatch8.stop();
+        System.out.println("Time QuickSort                = " + stopwatch8.getTime() + "ms");
         isSorted(inputArray8);
 
-        stopwatch.stop();
         long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         long actualMemUsed = afterUsedMem-beforeUsedMem;
 
         // System.out.println(inputArray[i]);
 
-        System.out.println("Time   = " + stopwatch.getTime() + "ms");
-
         // Dividing to convert Bytes into KB.
-        System.out.println("Memory = " + actualMemUsed/1024 + "KB");
+        System.out.println("Total Memory Used             = " + actualMemUsed/1024 + "KB");
 
     }
 
